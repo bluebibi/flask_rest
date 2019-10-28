c6cb3bcacec8a78eb8d1312c7d28d1ee9f71bcd6
Youn-Hee Han <yh21.han@gmail.com>
2019-10-28 10:55:54 +0900

    static 추가

--- application.py
+++ application.py
@@ -1,26 +1,44 @@
 #https://flask-restful.readthedocs.io/en/latest/
 
-from flask import Flask
+from flask import Flask, render_template, request
 from flask_restful import Api
+import logging
 
+from news import news_blueprint
 from rest_server.resource_check import resource_blueprint
 from rest_server.resource import TemperatureResource, TemperatureCreationResource, TemperatureByLocationResource
 
 application = Flask(__name__)
-application.register_blueprint(resource_blueprint)
+application.register_blueprint(news_blueprint, url_prefix='/news')
+application.register_blueprint(resource_blueprint, url_prefix='/resource')
+
 api = Api(application)
-
-
-@application.route("/")
-def say_hello(username="World"):
-    return '<p>Hello {0}!</p>'.format(username)
-
-
 api.add_resource(TemperatureResource, "/resource/<sensor_id>")
 api.add_resource(TemperatureCreationResource, "/resource/creation")
 api.add_resource(TemperatureByLocationResource, "/resource/location/<location>")
 
+logging.basicConfig(
+    filename='test.log',
+    level=logging.DEBUG
+)
+
+
+@application.route('/')
+def hello_html():
+    value = 50
+    value_list = ['파이썬', '자바', '스위프트']
+    return render_template(
+        'index.html',
+        name="yhhan",
+        value_list=value_list,
+        value=value
+    )
+
 
 if __name__ == "__main__":
+    logging.info("Flask Web Server Started!!!")
+
     application.debug = True
-    application.run()
+    application.config['DEBUG'] = True
+
+    application.run(host="localhost", port="8080")
